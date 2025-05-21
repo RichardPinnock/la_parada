d3f35b72f8a5e0e640e6a34e1a4bc9dee6d77923
Adrian <adrian.aguirre@prdavinci.tech>
2025-05-21 06:48:23 -0500

    feat: implement GET and POST endpoints for purchase management

---
+++ app/api/purchase/route.ts
@@ -0,0 +1,48 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+
+export async function GET() {
+    try {
+        // Obtiene todas las compras (purchase)
+        const purchases = await prisma.purchase.findMany();
+        return NextResponse.json(purchases, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Error al obtener las compras' }, { status: 500 });
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const {
+            userId,
+            total,
+            locationId,
+            items,
+        }: {
+            userId: string
+            total: number
+            locationId: string
+            items: { productId: string; quantity: number; unitPrice: number }[]
+        } = body
+        const newPurchase = await prisma.purchase.create({
+            data: {
+                userId,
+                total,
+                items: {
+                    create: items.map((item: any) => ({
+                        productId: item.productId,
+                        quantity: item.quantity,
+                        unitCost: item.unitPrice,
+                        totalCost: item.quantity * item.unitPrice,
+                        locationId,
+                    })),
+                },
+            },
+        });
+        return NextResponse.json(newPurchase, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Error al crear la compra' }, { status: 500 });
+    }
+}
