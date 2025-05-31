4b9c0495dec739f0c83bd1dd7246ad65d1fb74d7
Adrian <adrian.aguirre@prdavinci.tech>
2025-05-31 13:43:46 -0500

    feat: update GET function in products route - Refactor session handling and improve code readability. - Sort products by quantity in descending order before returning response. - Ensure consistent formatting in code style.

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,10 +4,10 @@ import { authOptions } from "@/auth";
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
   // console.log('session ===>>', session);
   let user = null;
-  if(session?.user?.id) {
+  if (session?.user?.id) {
     user = await prisma.user.findUnique({
       where: {
         id: session.user.id,
@@ -25,54 +25,59 @@ export async function GET(request: Request) {
       },
     });
   }
-  console.log('user ===>>', user);
-  console.log('location ===>>', user?.stockLocations[0].stockLocation.name);
-  console.log('location ===>>', user?.stockLocations[0].stockLocation.id);
-  
-  
+  console.log("user ===>>", user);
+  console.log("location ===>>", user?.stockLocations[0].stockLocation.name);
+  console.log("location ===>>", user?.stockLocations[0].stockLocation.id);
+
   const url = new URL(request.url);
   const page = parseInt(url.searchParams.get("page") || "1");
   const productsPerPage = 5;
   const offset = (page - 1) * productsPerPage;
 
-  const where = !session?.user?.id || user?.role == 'admin' ? {} 
-    : { locationId: { in: user?.stockLocations.map((location) => location.stockLocation.id) } }
+  const where =
+    !session?.user?.id || user?.role == "admin"
+      ? {}
+      : {
+          locationId: {
+            in: user?.stockLocations.map(
+              (location) => location.stockLocation.id
+            ),
+          },
+        };
 
   // Obtener productos paginados
   const products = await prisma.product.findMany({
     include: {
       warehouseStocks: {
-        where: where ,
+        where,
         include: {
           location: true,
         },
       },
-      
     },
     skip: offset,
     take: productsPerPage,
     orderBy: { id: "desc" },
   });
 
+  const sorted = products.sort((a, b) => {
+    const qtyA = a.warehouseStocks[0]?.quantity ?? 0;
+    const qtyB = b.warehouseStocks[0]?.quantity ?? 0;
+    return qtyB - qtyA; // descendente
+  });
+
   const totalProducts = await prisma.product.count();
   const totalPages = Math.ceil(totalProducts / productsPerPage);
 
-  return NextResponse.json({ products, totalPages });
+  return NextResponse.json({ products: sorted, totalPages });
 }
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { 
-      name,
-      purchasePrice,
-      salePrice,
-      imageName,
-      isActive,
-      notes,
-    } = body;
+    const { name, purchasePrice, salePrice, imageName, isActive, notes } = body;
     const product = await prisma.product.create({
-      data: { 
+      data: {
         name,
         purchasePrice,
         salePrice,
