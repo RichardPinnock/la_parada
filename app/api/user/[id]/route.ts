bc44ddbd81a5b2bb5106143594c8c9b8725cab22
Adrian <adrian.aguirre@prdavinci.tech>
2025-06-01 17:20:02 -0500

    feat: implement user creation and validation in POST endpoint - Added user creation logic with email validation and stock location checks, including password hashing for security.

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -6,8 +6,8 @@ export async function GET(request: NextRequest) {
     const url = new URL(request.url);
 
     const id = url.pathname.split("/").pop();
-    console.log('id ==>', id);
-    
+    console.log("id ==>", id);
+
     if (!id) {
       return NextResponse.json({ error: "ID inválido" }, { status: 400 });
     }
@@ -16,11 +16,11 @@ export async function GET(request: NextRequest) {
       where: { id },
       include: {
         stockLocations: {
-            select: {
-                stockLocation: true
-            }
+          select: {
+            stockLocation: true,
+          },
         }, // Incluir ubicaciones de stock
-      }
+      },
     });
 
     if (!user) {
@@ -37,6 +37,89 @@ export async function GET(request: NextRequest) {
       { error: "Error al obtener el usuario" },
       { status: 500 }
     );
-    
+  }
+}
+
+// put
+export async function PUT(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const id = url.pathname.split("/").pop();
+
+    if (!id) {
+      return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+    }
+
+    const body = await request.json();
+    const {
+      name,
+      email,
+      password,
+      stockLocationIds,
+      role,
+    }: {
+      name: string;
+      email: string;
+      password: string;
+      stockLocationIds: string[];
+      role: string;
+    } = body;
+
+    // Validar existencia del usuario
+    const existingUser = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { error: "Usuario no encontrado" },
+        { status: 404 }
+      );
+    }
+    // Validar existencia de ubicaciones de stock
+    const existingStockLocations = await prisma.stockLocation.findMany({
+      where: {
+        id: {
+          in: stockLocationIds,
+        },
+      },
+    });
+    if (existingStockLocations.length !== stockLocationIds.length) {
+      return NextResponse.json(
+        { error: "Una o más ubicaciones de stock no existen" },
+        { status: 404 }
+      );
+    }
+
+    // Actualizar usuario
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        name,
+        email,
+        password,
+        role,
+        stockLocations: {
+          set: stockLocationIds.map((stockLocationId: string) => ({
+            userId_stockLocationId: { userId: id, stockLocationId },
+          })),
+        },
+      },
+      include: {
+        stockLocations: {
+          select: {
+            stockLocation: true,
+          },
+        }, // Incluir ubicaciones de stock
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error("Error al actualizar el usuario:", error);
+    return NextResponse.json(
+      { error: "Error al actualizar el usuario" },
+      { status: 500 }
+    );
   }
 }
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 // GET: /api/user?limit=10&page=1
 export async function GET(request: Request) {
@@ -10,7 +11,6 @@ export async function GET(request: Request) {
   const search = searchParams.get("search") || "";
   const skip = (page - 1) * limit;
 
-  const where = search
   try {
     // Obtener usuarios con paginación
     const users = await prisma.user.findMany({
@@ -46,6 +46,68 @@ export async function GET(request: Request) {
     console.error("Error al obtener usuarios:", error);
     return NextResponse.json(
       { message: "Error al obtener los usuarios" },
+      { status: 500 }
+    );
+  }
+}
+
+// POST: /api/user
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, password, stockLocationIds, role = "dependiente" }: {
+      name: string;
+      email: string;
+      password: string;
+      stockLocationIds: string[];
+      role: string;
+    }  = body;
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },      
+    })
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "Existe un usuario con ese email, cámbielo" },
+        { status: 400 }
+      );
+    }
+     // Validar existencia de ubicaciones de stock
+    const existingStockLocations = await prisma.stockLocation.findMany({
+      where: {
+        id: {
+          in: stockLocationIds,
+        },
+      },
+    });
+    if (existingStockLocations.length !== stockLocationIds.length) {
+      return NextResponse.json(
+        { error: "Una o más ubicaciones de stock no existen" },
+        { status: 404 }
+      );
+    }
+    // Crear nuevo usuario
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        role,
+        password: await bcrypt.hash(password, 10),
+        stockLocations: {
+          createMany: {
+            data: stockLocationIds.map((id) => ({
+              stockLocationId: id,
+            })),
+          }
+        }
+      },
+    });
+
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    console.log("Error al crear usuario:", error);
+    return NextResponse.json(
+      { message: "Error al crear el usuario" },
       { status: 500 }
     );
   }
